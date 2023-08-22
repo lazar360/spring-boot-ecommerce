0213802350d75a2b35ec3a13d7226fe060fd6054
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-08-22 09:14:45 +0200

    create country repository & jsonignore on states

--- src/main/java/com/luv2code/ecommerce/entity/Country.java
+++ src/main/java/com/luv2code/ecommerce/entity/Country.java
@@ -1,5 +1,6 @@
 package com.luv2code.ecommerce.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,5 +22,6 @@ public class Country {
     @Column(name="name")
     private String name;
     @OneToMany(mappedBy = "country")
+    @JsonIgnore
     private List<State> states;
 }
