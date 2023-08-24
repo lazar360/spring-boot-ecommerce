ba47b0c67b50c4297fc8bc6ec6ed43ddd6a96e63
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-08-24 11:51:44 +0200

    maj checkout controller

--- src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
@@ -10,14 +10,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/checkout")
 public class CheckoutController {
 
-    private final CheckoutService checkoutService;
+    private CheckoutService checkoutService;
 
     public CheckoutController(CheckoutService checkoutService) {
         this.checkoutService = checkoutService;
     }
 
     @PostMapping("/purchase")
-    public PurchaseResponse placeOrder(@RequestBody Purchase purchase){
-        return checkoutService.placeOrder(purchase);
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
     }
+
 }
